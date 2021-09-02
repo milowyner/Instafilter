7f24e71fc8327994529e2600eb1dfeae418457a2
Milo Wyner <milowyner@gmail.com>
2021-09-02 12:51:36 -0700

    Action sheet example

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,22 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var blurAmount: CGFloat = 0
+    @State private var showingActionSheet = false
+    @State private var backgroundColor = Color.white
 
     var body: some View {
-        let blur = Binding<CGFloat> {
-            blurAmount
-        } set: {
-            blurAmount = $0
-            print("New value is \(blurAmount)")
-        }
-        
-        return VStack {
-            Text("Hello, World!")
-                .blur(radius: blurAmount)
-
-            Slider(value: blur, in: 0...20)
-        }
+        Text("Hello, World!")
+            .frame(width: 300, height: 300)
+            .background(backgroundColor)
+            .onTapGesture {
+                self.showingActionSheet = true
+            }
+            .actionSheet(isPresented: $showingActionSheet) {
+                ActionSheet(title: Text("Change background"), message: Text("Select a new color"), buttons: [
+                    .default(Text("Red")) { self.backgroundColor = .red },
+                    .default(Text("Green")) { self.backgroundColor = .green },
+                    .default(Text("Blue")) { self.backgroundColor = .blue },
+                    .cancel()
+                ])
+            }
     }
 }
 
