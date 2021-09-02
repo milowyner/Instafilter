13f92b6cf4ec64f182c2365b884b82d8baa8c0ec
Milo Wyner <milowyner@gmail.com>
2021-09-02 15:31:15 -0700

    Reset project to initial state to start on Instafilter

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -6,43 +6,11 @@
 //
 
 import SwiftUI
-import CoreImage
-import CoreImage.CIFilterBuiltins
-
-class ImageSaver: NSObject {
-    func writeToPhotoAlbum(image: UIImage) {
-        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
-    }
-    
-    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-        print("Save finished!")
-    }
-}
 
 struct ContentView: View {
-    @State private var image: Image?
-    @State private var inputImage: UIImage?
-    @State private var showingImagePicker = false
-
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-            
-            Button("Select Image") {
-                showingImagePicker = true
-            }
-        }
-        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage, content: {
-            ImagePicker(image: $inputImage)
-        })
-    }
-    
-    func loadImage() {
-        guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
-        ImageSaver().writeToPhotoAlbum(image: inputImage)
+        Text("Hello, world!")
+            .padding()
     }
 }
 
