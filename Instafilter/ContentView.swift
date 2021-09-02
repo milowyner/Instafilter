0e3ce4ab87f6f4623e1c3a4137f1e0fb1a9b5a12
Milo Wyner <milowyner@gmail.com>
2021-09-02 11:44:35 -0700

    Initial Commit

---
+++ Instafilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Milo Wyner on 9/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
