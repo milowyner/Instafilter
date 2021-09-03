c161a78701a0a8f099bc36f546f4434ec29f176c
Milo Wyner <milowyner@gmail.com>
2021-09-02 20:18:04 -0700

    Add applyProcessing function to ContentView that applies a filter to the chosen image when it's loaded and when the intensity slider is moved

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -6,15 +6,29 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
     @State private var image: Image?
     @State private var inputImage: UIImage?
     @State private var filterIntensity = 0.5
     @State private var showingImagePicker = false
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
     
     var body: some View {
-        NavigationView {
+        let intensity = Binding<Double>(
+            get: {
+                self.filterIntensity
+            },
+            set: {
+                self.filterIntensity = $0
+                self.applyProcessing()
+            }
+        )
+        
+        return NavigationView {
             VStack {
                 ZStack {
                     Rectangle()
@@ -36,7 +50,7 @@ struct ContentView: View {
 
                 HStack {
                     Text("Intensity")
-                    Slider(value: self.$filterIntensity)
+                    Slider(value: intensity)
                 }.padding(.vertical)
 
                 HStack {
@@ -61,7 +75,21 @@ struct ContentView: View {
     
     func loadImage() {
         guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
+        
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgimg)
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
